fd2c2865b6854b4e5c87e01c4dea0a89ec5901a0
Luis Felipe Carvajal Retana <61105182+felipec2198@users.noreply.github.com>
2021-02-09 21:16:20 -0600

    Add files via upload

---
+++ 2021-02-09_tidytuesday.R
@@ -0,0 +1,20 @@
+library(tidytuesdayR)
+library(ggplot2)
+library(scales)
+library(ggthemes)
+
+
+student_debt <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/student_debt.csv')
+
+student_debt %>%
+  ggplot(aes(x = year, y = loan_debt, color = race)) +
+  geom_line() +
+  scale_y_continuous(labels = dollar) +
+  expand_limits(y = 0) +
+  labs(title = "Average family student loan debt for aged 25-55",
+       subtitle = "By race and year normalized to 2016 dollars.",
+       x = "Year",
+       y = "Loan debt",
+       color = "Race") +
+  ggthemes::theme_clean()
+  
